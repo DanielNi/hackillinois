49c69d503f0a0bf4c519105b883da7b03089b392
Daniel Ni <danielni@wustl.edu>
2014-04-12 09:39:50 -0500

    fixed json

--- event.js
+++ event.js
@@ -13,3 +13,22 @@ chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}
       alert(tabs[0].url);
    }
 );
+
+// var successURL = 'https://www.facebook.com/connect/login_success.html';
+// function onFacebookLogin() {
+//                 if (!localStorage.accessToken) {
+//                     chrome.tabs.getAllInWindow(null, function(tabs) {
+//                         for (var i = 0; i < tabs.length; i++) {
+//                             if (tabs[i].url.indexOf(successURL) == 0) {
+//                                 var params = tabs[i].url.split('#')[1];
+// 				access = params.split('&')[0]
+//                                 console.log(access);
+//                                 localStorage.accessToken = access;
+//                                 chrome.tabs.onUpdated.removeListener(onFacebookLogin);
+//                                 return;
+//                             }
+//                         }
+//                     });
+//                 }
+//             }
+//             chrome.tabs.onUpdated.addListener(onFacebookLogin);
--- popup.js
+++ popup.js
@@ -5,3 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
 chrome.tabs.getSelected(null, function(tab) {
     document.getElementById('currentLink').innerHTML = tab.url;
 });
+
+if (localStorage.accessToken) {
+    var graphUrl = "https://graph.facebook.com/me?" + localStorage.accessToken + "&callback=displayUser";
+    console.log(graphUrl);
+
+    var script = document.createElement("script");
+    script.src = graphUrl;
+    document.body.appendChild(script);
+
+    function displayUser(user) {
+        console.log(user);
+    }
+}
