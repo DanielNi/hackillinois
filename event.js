1af8490a058688205c48aed7593525e932263781
Daniel Ni <danielni@wustl.edu>
2014-04-12 18:49:57 -0500

    separated css, new icon

--- event.js
+++ event.js
@@ -9,26 +9,90 @@
 // });
 
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tabs){
-      alert(tabs[0].url);
-   }
-);
+	function(tabs){
+      // alert(tabs[0].url);
+  	}
+  );
 
-// var successURL = 'https://www.facebook.com/connect/login_success.html';
-// function onFacebookLogin() {
-//                 if (!localStorage.accessToken) {
-//                     chrome.tabs.getAllInWindow(null, function(tabs) {
-//                         for (var i = 0; i < tabs.length; i++) {
-//                             if (tabs[i].url.indexOf(successURL) == 0) {
-//                                 var params = tabs[i].url.split('#')[1];
-// 				access = params.split('&')[0]
-//                                 console.log(access);
-//                                 localStorage.accessToken = access;
-//                                 chrome.tabs.onUpdated.removeListener(onFacebookLogin);
-//                                 return;
-//                             }
-//                         }
-//                     });
-//                 }
-//             }
-//             chrome.tabs.onUpdated.addListener(onFacebookLogin);
+// var successURL = 'https://www.facebook.com/connect/*';
+// //close tab after redirect to success
+// function onFacebookLogin() { // doesn't work yet
+//     if (localStorage.accessToken) {
+//         chrome.tabs.query({url: successURL}, function(tabs) {
+//             chrome.tabs.remove(0);
+//         });
+//     }
+// }
+// chrome.tabs.onUpdated.addListener(onFacebookLogin);
+
+// window.fbAsyncInit = function() {
+// 	FB.init({
+// 		appId      : '{your-app-id}',
+// 		status     : true,
+// 		xfbml      : true
+// 	});
+// };
+
+// (function(d, s, id){
+// 	var js, fjs = d.getElementsByTagName(s)[0];
+// 	if (d.getElementById(id)) {return;}
+// 	js = d.createElement(s); js.id = id;
+// 	js.src = "https://connect.facebook.net/en_US/all.js";
+// 	fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+
+// window.fbAsyncInit = function() {
+//         // init the FB JS SDK
+//         FB.init({
+//             appId: 'APP_ID', // App ID from the App Dashboard
+//             //channelUrl : '//www.example.com/', // Channel File for x-domain communication
+//             status: true, // check the login status upon init?
+//             cookie: true, // set sessions cookies to allow your server to access the session?
+//             xfbml: true  // parse XFBML tags on this page?
+//         });
+
+//         // Additional initialization code such as adding Event Listeners goes here
+//         console.log(FB);
+
+//     };
+
+//     // Load the SDK's source Asynchronously
+//     (function(d, debug) {
+//     	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+//     	if (d.getElementById(id)) {
+//     		return;
+//     	}
+//     	js = d.createElement('script');
+//     	js.id = id;
+//     	js.async = true;
+//     	js.src = "https://connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
+//     	ref.parentNode.insertBefore(js, ref);
+//     }(document, /*debug*/false));
+
+
+var ref = new Firebase('https://sweltering-fire-4327.firebaseio.com/');
+var users = ref.child('users');
+
+var auth = new FirebaseSimpleLogin(ref, function(error, user) { 
+	if (error) { // an error occurred while attempting login
+    	console.log(error);
+  	} else if (user) { // user authenticated with Firebase
+    	console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+  		var userid = user.id;
+  		var userRef = users.child(userid);
+  		var friends = userRef.child('friends');
+  		var hasVisited = userRef.child('hasVisited');
+
+  		// facebook API shjit
+  	} else { // user is logged out
+  	}
+
+
+});
+
+function onLoginButtonClicked() {
+	auth.login('facebook', {
+ 		rememberMe: true,
+  		scope: 'user_friends'
+	});
+}
--- popup.js
+++ popup.js
@@ -1,20 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
   alert("loaded");
+  document.getElementById('alertButton').addEventListener('click', onLoginButtonClicked);
 });
 
 chrome.tabs.getSelected(null, function(tab) {
-    document.getElementById('currentLink').innerHTML = tab.url;
+  document.getElementById('currentLink').innerHTML = tab.url;
 });
 
 if (localStorage.accessToken) {
-    var graphUrl = "https://graph.facebook.com/me?" + localStorage.accessToken + "&callback=displayUser";
-    console.log(graphUrl);
+  var graphUrl = "https://graph.facebook.com/me?" + localStorage.accessToken + "&callback=displayUser";
+  console.log(graphUrl);
 
-    var script = document.createElement("script");
-    script.src = graphUrl;
-    document.body.appendChild(script);
+  var script = document.createElement("script");
+  script.src = graphUrl;
+  document.body.appendChild(script);
 
-    function displayUser(user) {
-        console.log(user);
-    }
+  function displayUser(user) {
+    console.log(user);
+  }
 }
+
+// var facebookAuth = new OAuth2('facebook', {
+//   client_id: '768415276511807',
+//   client_secret: '28e40d0bc03186cdfbd97552891f4a17',
+//   api_scope: 'basic_info'
+// });
+
+// facebookAuth.authorize(function() {
+//   var xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = function(event) {
+//     if (xhr.readyState == 4) {
+//       if(xhr.status == 200) {
+//         var parsed = JSON.parse(xhr.responseText);
+//         var html = '';
+//         parsed.data.forEach(function(item, index) {
+//           html += '<li>' + item.name + '</li>';
+//         });
+//         document.querySelector('#friends').innerHTML = html;
+//         return;
+//       } else {
+//         //request failure
+//       }
+//     }
+//   }
+// });
+
+// xhr.open('GET', 'https://graph.facebook.com/me/picture', true);
+// xhr.setRequestHeader('Content-Type', 'application/json');
+// xhr.setRequestHeader('Authorization', 'OAuth ' + facebook.getAccessToken());
+
+// xhr.send();
